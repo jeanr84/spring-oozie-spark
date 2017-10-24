535609fca9aea30a3ee053d932596edda8c617af
rjean84 <robinjeanr@gmail.com>
2017-10-24 22:55:06 +0200

    Add Oozie code (need to be tested)

--- src/main/java/com/rjean84/springooziespark/controller/OozieController.java
+++ src/main/java/com/rjean84/springooziespark/controller/OozieController.java
@@ -3,10 +3,13 @@ package com.rjean84.springooziespark.controller;
 import com.rjean84.springooziespark.model.WorkflowParameters;
 import com.rjean84.springooziespark.service.OozieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class OozieController {
@@ -17,6 +20,11 @@ public class OozieController {
     @PostMapping("/oozie/submit")
     public ResponseEntity<String> submitWorkflow(@RequestBody WorkflowParameters workflowParameters) {
         //TODO : return the Oozie id
-        return ResponseEntity.ok(oozieService.submitWorkflow(workflowParameters));
+        Optional<String> jobIdOptional = oozieService.submitWorkflow(workflowParameters);
+        if (jobIdOptional.isPresent()) {
+            return ResponseEntity.ok(jobIdOptional.get());
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
--- src/main/java/com/rjean84/springooziespark/model/WorkflowParameters.java
+++ src/main/java/com/rjean84/springooziespark/model/WorkflowParameters.java
@@ -5,7 +5,8 @@ import lombok.Data;
 @Data
 public class WorkflowParameters {
 
+    private String applicationName;
+    private String applicationPath;
     private String arg1;
     private String arg2;
-    private String applicationName;
 }
